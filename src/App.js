ba3e74daf41bd7638c6681f9cdd82a72a2b893c7
Josh Gutierrez <joshgutierrez@Joshs-MacBook-Air.local>
2020-06-04 10:22:49 -0500

    framework started

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      // value: event.target.value,
+      [event.target.name]: event.target.value,
+    });
+  }
+  handleSubmit (event)  {
+    // don't reload the page
+    event.preventDefault();
+    this.setState({
+      username: '',
+      password: ''
+    });
+    
+  };
+
+  // handleClick = () => {
+  //   this.setState({
+  //     username: !this.state.username,
+  //     password: !this.state.password
+  //   })
+  // }
+
+  render() {
+    return (
+      <div className="App">
+        
+        <form>
+          <TextField
+            name="username"
+            type="text"
+            placeholder="Username"
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+
+          <TextField
+            placeholder="Password"
+            name = "password"
+            type="text"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+        </form>
+
+        <Button variant="contained" color="primary" onClick ={this.handleSubmit}>
+          Primary
+        </Button>
+      </div>
+    );
+  }
 }
 
 export default App;
