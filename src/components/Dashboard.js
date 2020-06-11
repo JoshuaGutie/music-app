6338c91e9afba1739f6bdfc85ff1fb35831edf34
Josh Gutierrez <joshgutierrez@Joshs-MacBook-Air.local>
2020-06-11 11:23:05 -0500

    array pushes

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -10,101 +10,120 @@ import Switch from "@material-ui/core/Switch";
 import Typography from "@material-ui/core/Typography";
 
 class Dashboard extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			online: false,
-			quality: 20,
-			volume: 10
-		};
-		this.handleChange = this.handleChange.bind(this);
-		this.handleChange2 = this.handleChange2.bind(this);
-		this.handleClick = this.handleClick.bind(this);
-		// this.handleClick2 = this.handleClick2.bind(this);
-	}
-	handleChange = (event, volume) => {
-		this.setState({ volume });
-	};
-	handleChange2 = (event) => {
-		this.setState({ quality: event.target.value });
-		console.log(event.target.value);
-	};
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: false,
+      quality: 20,
+      volume: 10,
+      notifications: [],
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleChange2 = this.handleChange2.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    // this.handleClick2 = this.handleClick2.bind(this);
+  }
+  // let popUp = ['Your application is offline. You wont be able to share or stream music to other devices.',
+  // 			 'Listening to music at a high volume could cause long-term hearing loss.',
+  // 			 'Music quality is degraded. Increase quality if your connection allows it.']
 
-	handleClick = () => {
-		this.setState({
-			online: !this.state.online
-		});
-	};
-	// handleClick2 = () => {
-	// 	this.setState({
-	// 		quality: this.state.value
-	// 	});
-	// };
+  componentDidUpdate(prevProps, prevState) {
+    let popUp = [
+      "Your application is offline. You wont be able to share or stream music to other devices.",
+    ];
+    if (prevState.online == true) {
+      console.log("Online has changed!!!", this.state.online);
+      console.log(this.state.notifications);
+      this.state.notifications = popUp;
+    }
+  }
 
-	render() {
-		// debugger;
-		const { volume } = this.state;
-		const { quality } = this.state;
-		return (
-			<div className="App">
-				<AppBar style={{ height: "10%" }}>My Music App </AppBar>
-				<br />
-				<h1>WELCOME USER</h1>
-				<div style={{ display: "flex" }}>
-					<Card variant="elevation">
-						<CardContent>
-							<h2>Online Mode</h2>
+  handleChange = (event, volume) => {
+    this.setState({ volume });
+  };
+  handleChange2 = (event) => {
+    this.setState({ quality: event.target.value });
+    console.log(event.target.value);
+  };
 
-							<Typography variant="body2" component="p">
-								Is this application connected to the internet?
-							</Typography>
-							<CardActions>
-								<Switch onClick={this.handleClick} />
-								{this.state.online ? true : false}
-							</CardActions>
-						</CardContent>
-					</Card>
+  handleClick = () => {
+    this.setState({
+      online: !this.state.online,
+    });
+  };
+  // handleClick2 = () => {
+  // 	this.setState({
+  // 		quality: this.state.value
+  // 	});
+  // };
 
-					<Card variant="elevation">
-						<CardContent>
-							<h2>Master Volume</h2>
+  render() {
+    // debugger;
+    const { volume } = this.state;
+    const { quality } = this.state;
+    return (
+      <div className="App">
+        <AppBar style={{ height: "10%" }}>My Music App </AppBar>
+        <br />
+        <h1>WELCOME USER</h1>
+        <div style={{ display: "flex" }}>
+          <Card variant="elevation">
+            <CardContent>
+              <h2>Online Mode</h2>
 
-							<Typography variant="body2" component="p">
-								Overrides all other sound settings in this application.
-							</Typography>
-							<CardActions>
-								<Slider
-									defaultValue={10}
-									step={10}
-									marks
-									min={0}
-									max={100}
-									valueLabelDisplay="auto"
-									volume={volume}
-									onChange={this.handleChange}
-								/>
-							</CardActions>
-						</CardContent>
-					</Card>
-					<Card variant="elevation">
-						<CardContent>
-							<h2>Sound Quality</h2>
-							<Typography variant="body2" component="p">
-								Manually control the music quality in event of poor connection.
-							</Typography>
-						</CardContent>
-						<CardActions>
-							<Select native value={quality} onChange={this.handleChange2}>
-								<option value={10}>Low</option>
-								<option value={20}>Normal</option>
-								<option value={30}>High</option>
-							</Select>
-						</CardActions>
-					</Card>
-				</div>
-			</div>
-		);
-	}
+              <Typography variant="body2" component="p">
+                Is this application connected to the internet?
+              </Typography>
+              <CardActions>
+                <Switch onClick={this.handleClick} />
+                {this.state.online ? true : false}
+              </CardActions>
+            </CardContent>
+          </Card>
+
+          <Card variant="elevation">
+            <CardContent>
+              <h2>Master Volume</h2>
+
+              <Typography variant="body2" component="p">
+                Overrides all other sound settings in this application.
+              </Typography>
+              <CardActions>
+                <Slider
+                  defaultValue={10}
+                  step={10}
+                  marks
+                  min={0}
+                  max={100}
+                  valueLabelDisplay="auto"
+                  volume={volume}
+                  onChange={this.handleChange}
+                />
+              </CardActions>
+            </CardContent>
+          </Card>
+          <Card variant="elevation">
+            <CardContent>
+              <h2>Sound Quality</h2>
+              <Typography variant="body2" component="p">
+                Manually control the music quality in event of poor connection.
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Select native value={quality} onChange={this.handleChange2}>
+                <option value={10}>Low</option>
+                <option value={20}>Normal</option>
+                <option value={30}>High</option>
+              </Select>
+            </CardActions>
+          </Card>
+          <div>
+            <h1>System notifications:</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Dashboard;
